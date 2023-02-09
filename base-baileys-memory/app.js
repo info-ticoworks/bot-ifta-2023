5c2b0409be376a98892a14c384d2c28d7932bc7b
Cesar Segura <cesar.segura@ucem.ac.cr>
2023-02-09 01:24:12 -0500

    Prueba de bot actualizado

---
+++ base-baileys-memory/app.js
@@ -0,0 +1,23 @@
+const 
+    { createProvider,
+     } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+const { createBotDialog } = require('@bot-whatsapp/contexts/dialogflow')
+
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBotDialog({
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
